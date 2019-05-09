2f05c9cae809ac6fef6da437ee6043a6730f2e34
Namnextx <phuongnam.nextx@gmail.com>
2019-05-09 15:45:56 +0700

    - Create Model: done

---
+++ src/main/java/com/codegym/cms/model/Category.java
@@ -0,0 +1,60 @@
+package com.codegym.cms.model;
+
+import javax.persistence.*;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    @OneToMany(targetEntity = Note.class)
+    private List<Note> noteList;
+
+    public Category() {
+    }
+
+    public Category(String name, String description, List<Note> noteList) {
+        this.name = name;
+        this.description = description;
+        this.noteList = noteList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Note> getNoteList() {
+        return noteList;
+    }
+
+    public void setNoteList(List<Note> noteList) {
+        this.noteList = noteList;
+    }
+}
---
+++ src/main/java/com/codegym/cms/model/Note.java
@@ -0,0 +1,58 @@
+package com.codegym.cms.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "node")
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public Note() {
+    }
+
+    public Note(String title, String content, Category category) {
+        this.title = title;
+        this.content = content;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
